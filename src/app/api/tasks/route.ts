fc18d02ce40eb009105b9844781f04962a29bd00
JSEAN6 <seannjoroge0@gmail.com>
2026-02-14 21:09:03 +0300

    add attach tasks to logged-in user

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,21 +1,44 @@
-import { NextResponse } from "next/server";
-import { db } from "@/app/db"; // your drizzle instance
+import { auth } from "@/auth";
+import { db } from "@/app/db";
 import { tasks } from "@/app/db/schema";
+import { eq } from "drizzle-orm";
 
-export async function GET() {
-  const allTasks = await db.select().from(tasks);
-  return NextResponse.json(allTasks);
-}
 
-export async function POST(req: Request) {
+export async function POST(req: Request){
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+
+  if (!session) {
+    return new Response("Unauthorised", { status: 401});
+  }
+
   const body = await req.json();
 
   const newTask = await db.insert(tasks).values({
     title: body.title,
     description: body.description,
-    dueAt: body.dueAt ? new Date(body.dueAt) : null,
+    dueAt: body.dueAT ? new Date(body.dueAt): null,
     completed: body.completed ?? false,
+    userId: session.user.id,
   }).returning();
 
-  return NextResponse.json(newTask[0]);
+  return Response.json(newTask[0]);
+}
+
+export async function GET(req: Request) {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  
+  const userTasks = await db
+   .select()
+   .from(tasks)
+   .where(eq(tasks.userId, session.user.id));
+
+   return Response.json(userTasks)
 }
