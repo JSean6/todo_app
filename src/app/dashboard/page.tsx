5cf6fd0976dd479a8c857f3825a7fb971e76f605
JSEAN6 <seannjoroge0@gmail.com>
2026-03-05 13:56:36 +0300

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,24 @@
 import { Logout } from "@/components/logout"
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { db } from "@/app/db";
+import { task } from "@/app/db/schema";
+import { eq } from "drizzle-orm";
+export default async function Dashboard() {
+    const session = await auth.api.getSession();
 
-export default function Dashboard() {
+    if (!session) {
+        redirect("/login");
+    }
+
+    const tasks = await db
+        .select()
+        .from(task)
+        .where(eq(task.userId, session.user.id))
     return (
-    <div>
+    <div style={{ padding: "2rem"}}>
+        <h2>Your Tasks</h2>
+        
         <Logout />
     </div>
 );
