cab0778aaccf8f07a948fdca45e918df1a3c2e81
Levi Opunga <leviopunga@Levis-MacBook-Air.local>
2026-03-07 12:37:04 +0300

    fix add task

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { db } from "@/app/db";
-import { task } from "@/app/db/schema";
+import { task as taskTable } from "@/app/db/schema";
 import { NextResponse } from "next/server";
 import { eq } from "drizzle-orm";
 
@@ -20,8 +20,31 @@ export async function GET() {
 
   const tasks = await db
     .select()
-    .from(task)
-    .where(eq(task.userId, session.user.id));
+    .from(taskTable)
+    .where(eq(taskTable.userId, session.user.id));
 
   return NextResponse.json(tasks);
+}
+
+
+export async function POST(request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { title, description, dueAt } = await request.json();
+  
+  const task = await db.insert(taskTable).values({
+    id: crypto.randomUUID(),
+    title,
+    description,
+    dueAt: dueAt ? new Date(dueAt) : null,
+    userId: session.user.id,
+  });
+
+  return NextResponse.json(task);
 }
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -27,7 +27,6 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { signUp } from "../../server/users"
 import { z } from "zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
@@ -64,16 +63,17 @@ export function SignupForm({
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
-    const { success, message } = await signUp(
-      values.email, 
-      values.password,
-      values.username
-    )
-    if (success){
-      toast.success(`${message as string} Check email for verification`);
+    const { data, error } = await authClient.signUp.email({
+      email: values.email,
+      password: values.password,
+      name: values.username,
+      callbackURL: "/dashboard",
+    })
+    if (data){
+      toast.success("Signed up successfully");
       router.push("/dashboard");
     } else {
-      toast.error(message as string);
+      toast.error(error?.message as string);
     }
     setIsLoading(false);
   }
